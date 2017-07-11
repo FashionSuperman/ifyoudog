c4985c26a01b16089809b99354b4a4d7fbf77f6f
1462989455@qq <1462989455@qq>
2017-07-11 15:57:20 +0800

    微信支付

--- assets/Script/ButtonControl.js
+++ assets/Script/ButtonControl.js
@@ -1,4 +1,5 @@
 var properties = require("Properties");
+var netTool = require("Net");
 cc.Class({
     extends: cc.Component,
 
@@ -39,6 +40,10 @@ cc.Class({
             type : cc.Node
         },
         leavecontent:{
+            default : null,
+            type : cc.Node
+        },
+        wxpaynode:{
             default : null,
             type : cc.Node
         }
@@ -208,14 +213,107 @@ cc.Class({
      */
     wxpaybutton : function(event){
         //金额弹窗
-        
+        this.wxpaynode.x = 0;
+        this.wxpaynode.y = 0;
+        this.wxpaynode.zIndex = 10;
     },
 
     /**
      * 调用微信支付接口
      */
-    wxpay : function(){
+    wxpay : function(event){
+       var node = event.target;
+       var buttonNmae = node.name;
+       switch(buttonNmae){
+            case "one":
 
+                break;
+            case "two":
+
+                break;
+            case "three":
+
+                break;
+            case "four":
+
+                break;
+            case "five":
+
+                break;
+            case "fourty":
+
+                break;
+            case "sixty":
+
+                break;
+            case "eighty":
+
+                break;
+            case "onehundred":
+
+                break;
+            default:
+                break;         
+       }
+    },
+    
+    wxpayCommon : function(paycount){
+        var that = this;
+        var method = "POST";
+        if(!paycount || paycount < 0){
+            paycount = 1;
+        }
+
+        var OrderUrlData = {
+            "note" : "充值"
+        };
+
+        netTool.sendRequest(properties.url.generateUserOrder,method,OrderUrlData,function(data) {
+		
+            data = JSON.parse(data);
+            
+            var orderid = data.responseData.orderid;
+            // 调用服务获取调起微信支付的参数
+            var ApplyWXPayUrlData = {
+                "total_fee" : paycount,
+                "orderid" : orderid
+            };
+            sendRequest(properties.url.applyWXPayUrl, method, ApplyWXPayUrlData, function(data) {
+                data = JSON.parse(data);
+                
+                
+                var resApplyWXPayUrl = data;
+                resApplyWXPayUrl.package = resApplyWXPayUrl.backage;
+                delete resApplyWXPayUrl.backage;
+                that.applaywxmethod(resApplyWXPayUrl);
+            });
+        });
+    },
+
+
+
+
+    onBridgeReady : function(){
+        WeixinJSBridge.invoke('getBrandWCPayRequest', data, function(res) {
+            if (res.err_msg == "get_brand_wcpay_request:ok") {// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回
+                                                                // ok，但并不保证它绝对可靠。
+
+            }
+        });
+    },
+
+    applaywxmethod : function(data){
+        if (typeof WeixinJSBridge == "undefined") {
+            if (document.addEventListener) {
+                document.addEventListener('WeixinJSBridgeReady', onBridgeReady,
+                        false);
+            } else if (document.attachEvent) {
+                document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+            }
+        } else {
+            onBridgeReady(data);
+        }
     }
 
 
--- assets/Script/Properties.js
+++ assets/Script/Properties.js
@@ -1,9 +1,11 @@
 var url = {};
-url.serverPath = "http://192.168.1.103:8088/fs-game-dubbox-web/fs-game-service/";
+url.serverPath = "http://www.fashionsuperman.top/ifyoudog/fs-game-service/";
 url.url_getShopCommodities = url.serverPath + "Shop/getShopCommodities";
 url.buyUrl = url.serverPath + "Shop/buyShopCommodity";
 url.url_getUserPackageList = url.serverPath + "Package/getUserPackageList";
 url.getLoginUserInfo = url.serverPath + "User/getLoginUserInfo";
 url.wxlogin = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx324313fc147c74e2&redirect_uri=http%3a%2f%2fwww.fashionsuperman.top%2fifyoudog%2ffs-game-service%2fUser%2floginwx&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+url.generateUserOrder = url.serverPath + "Order/generateUserOrder";
+url.applyWXPayUrl = url.serverPath + "DogBiz/applyWXPay";
 
 module.exports.url = url;
